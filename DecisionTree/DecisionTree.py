88e9720c88c34280c480f90b37e32f496b54fd81
King-Kash <kashyap.kanumuri@gmail.com>
2025-04-24 00:08:23 -0700

    5/24/2025_12:08

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,3 +1,5 @@
+import numpy as np
+from collections import Counter
 
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
@@ -29,10 +31,71 @@ class DecisionTree:
             self.n_features = X.shape[1]
         else:
             self.n_features = min(X.shape[1], self.n_features)
-            
+
         self.root = self.grow_tree(X, y)
+    
+    def _grow_tree(self, X, y, depth=0):
+        #depth keeps track current tree depth
+        n_samples, n_feats = X.shape
+        n_lables = len(np.unique(y))
+
+        #check stopping criteria:
+        if (depth>=self.max_depth or n_samples<self.min_sample_split or n_lables==1):
+            #n_labels == 1 means node has only one label -> its a leaf node do not split it
+            #if a given node has less than the min number of samples required to split. -> do not split any further to prevent overfitting
+            #if depth of tree exceeds max_depth we decide -> do not split any further
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+        
+        #pick some of the features out of all the available features without replacement. how many = self.n_features. done for randomness
+        feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
+        #find the best feature and then since the feature is numerical the best value of that feature (the threshold) to split the data on
+        best_features, best_thresh = self._best_split(X, y, feat_idxs)
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        value = counter.most_common(1)[0][0]
+        return value
+    
+    def _best_split(self, X, y, feat_idxs):
+        best_information_gain = -1
+        split_feature_idx, split_threshold = None, None
+        
+        #2 hyper params the feature we want to split on and since that feature is numerical, the threshold we want to sliton
+        #thif function tries every possible combination of the two hyperparameters finding the best combination for maximizing the information gain
+        for feat_idx in feat_idxs:
+            #one column of X containg the values of the feature at feat_idx for each sample.
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column) #all the options we have for where we want the split to be.
+
+            for thr in thresholds:
+                #calculate information gain for current feature, threshold combination
+                gain = self._information_gain(y, X_column, thr)
+                if gain > best_information_gain:
+                    best_information_gain = gain
+                    split_feature_idx = feat_idx
+                    split_threshold = thr
+
+    def _information_gain(self, y, X_column, threshold):
+        #parent entropy
+        parent_entropy = self._entropy(y)
+
+        #create children
+
+        #calculate wegihted avg. of entropy of children
+
+        #calculate information gain
+    
+    def _entropy(self, y):
+        #bincount creates a histogram of relative frequency for each element that appears in y
+        histogram = np.bincount(y)
+        probabilities = histogram / len(y) #calculate probability of each possible y occuring all at once and store in array
+        return -np.sum([prob * np.log(prob) for prob in probabilities if prob>0]) #sum up all the probabilities
 
 
     def predict():
         pass
+
+
+
 
