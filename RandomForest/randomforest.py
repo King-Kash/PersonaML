699b18faf4c1cd9fdeb731361a542308d9916794
King-Kash <kashyap.kanumuri@gmail.com>
2025-05-25 00:53:11 -0700

    5/25/2025_12:52
    Random Forrest implementation complete.

--- RandomForest/randomforest.py
+++ RandomForest/randomforest.py
@@ -1,5 +1,6 @@
-from ..DecisionTree import DecisionTree
+from PersonaML.DecisionTree.DecisionTree import DecisionTree
 import numpy as np
+from collections import Counter
 
 class RandomForest:
     def __init__(self, n_trees=10, max_depth=10, min_samples=2, n_features=None):
@@ -12,8 +13,8 @@ class RandomForest:
     def fit(self, X, y):
         self.trees = []
         for _ in range(self.n_trees):
-            tree = DecisionTree(max_depth=self.max_depth, 
-                                min_samples_split=self.min_samples_split,
+            tree = DecisionTree(max_depth=self.max_depth,
+                                min_sample_split=self.min_samples_split, 
                                 n_features=self.n_features)
             X_sample, y_sample = self._bootstrap_sampling(X, y)
             tree.fit(X_sample, y_sample)
@@ -25,9 +26,23 @@ class RandomForest:
         idxs = np.random.choice(n_samples, n_samples, replace=True)
         return X[idxs], y[idxs]
     
+    #majority vote
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+    
     def predict(self, X):
         for tree in self.trees:
             #list comprehension get used to it.
             predictions = np.array([tree.predict(X) for tree in self.trees])
-            
+            '''[ [1, 0, 1, 1],
+                 [0, 0, 1, 0], ...] #each tree produces a list of predictions
+            # we need to reformat this array to be in this form
+                #[ [predictions for sample one from each tree], [], [] ]'''
+            #this takes the cols which correspond to the prediction for a particular sample by all the trees and makes it a row.
+            trees_preds = np.swapaxes(predictions, 0, 1)
+            return np.array([self._most_common_label(pred) for pred in trees_preds])
+
+
             
---
+++ RandomForest/test.py
@@ -0,0 +1,27 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+from PersonaML.RandomForest.randomforest import RandomForest
+
+data = datasets.load_breast_cancer()
+X, y = data.data, data.target
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
+
+def accuracy(predictions, y_test):
+    correct_score = 0
+    for idx, y in enumerate(predictions):
+        if y == y_test[idx]:
+            correct_score+=1
+    return correct_score / len(y_test)
+
+
+classifier = RandomForest() #using all defaults
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+
+acc = accuracy(predictions, y_test)
+print("Model Accuracy:", acc)
